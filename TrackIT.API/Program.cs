602ec766e2ed2a2a5f33f57f19bfe93099c85d34
Ateeb Shahid <eauditore12341@gmail.com>
2026-01-18 18:21:33 +0500

    Health endpoint

--- TrackIT.API/Program.cs
+++ TrackIT.API/Program.cs
@@ -92,7 +92,7 @@ builder.Services.AddCors(options =>
 
 
 var app = builder.Build();
-
+app.MapGet("/health", () => Results.Ok("Alive"));
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
